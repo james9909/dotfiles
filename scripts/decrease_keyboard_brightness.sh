fcc2b2f7c5b054cd341b32af069658323bac2142
James Wang <jameswang9909@hotmail.com>
2015-11-02 17:58:11 -0500

    Add scripts to increase/decrease screen and keyboard brightness

---
+++ scripts/decrease_keyboard_brightness.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+if [[ $brightness > 0 ]]; then
+    let brightness=$brightness-1
+    echo "echo $brightness > /sys/class/leds/asus::kbd_backlight/brightness" | sudo zsh
+fi
