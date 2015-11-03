b6f5f1ca5b12815e64b526b4b6965657c0cd75fd
James Wang <jameswang9909@hotmail.com>
2015-11-03 09:01:55 -0500

    Fix a few sytax errors

--- scripts/decrease_keyboard_brightness.sh
+++ scripts/decrease_keyboard_brightness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+brightness=$(cat /sys/class/leds/asus::kbd_backlight/brightness)
 if [[ $brightness > 0 ]]; then
     let brightness=$brightness-1
     echo "echo $brightness > /sys/class/leds/asus::kbd_backlight/brightness" | sudo zsh
