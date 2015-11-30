5ea9396edaa33d093f55ce0a9b2c64dcbbbbb0e2
James Wang <jameswang9909@hotmail.com>
2015-11-30 11:34:52 -0500

    Add script to update school vim

---
+++ school/update_vim.sh
@@ -0,0 +1,45 @@
+function update_vim() {
+    first_clone=0
+    curr_dir=$(pwd)
+
+    # Install ncurses and vim if nonexistant
+    if [[ ! -d "$HOME/vim" ]]; then
+        cd ~
+        curl -O https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz
+        mkdir ~/usr/local
+        tar xzvf ncurses-6.0.tar.gz
+        cd ncurses-6.0
+        ./configure --prefix=$HOME/usr/local
+        make
+        make install
+        rm -rf ncurses-6.0
+        git clone https://github.com/vim/vim
+        first_clone=1
+        echo "Cloned vim"
+    fi
+
+    cd ~/vim/src
+    git fetch
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @{u})
+
+    if [ $LOCAL = $REMOTE -a $first_clone = 0 ]; then
+        echo "Vim is up to date"
+        cd $curr_dir
+        return
+    else
+        git pull
+        make distclean
+        LDFLAGS=-L$HOME/usr/local/lib ./configure  --prefix=$HOME/usr/local
+        make
+        make install
+    fi
+    version=$(vim --version | grep Vi\ IMproved)
+    patches=$(vim --version | grep patches)
+    echo "Vim is now updated"
+    echo $version
+    echo $patches
+    cd $curr_dir
+}
+
+update_vim
