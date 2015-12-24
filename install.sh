e7755cf543edb3193b1931c1d4a6ce84c0e132cb
James Wang <jameswang9909@hotmail.com>
2015-12-23 19:42:43 -0500

    Add rofi to install script

--- install.sh
+++ install.sh
@@ -157,6 +157,7 @@ read ans
 if [[ $ans =~ ^[Yy]$ ]]; then
     sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev
     sudo apt-get install -y i3status
+    sudo apt-get install -y rofi
     git clone https://github.com/Airblader/i3 ~/i3-gaps
     cd ~/i3-gaps
     make
