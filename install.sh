81d8eb38a031c9f25d49ea876019e3807bed4db5
James Wang <jameswang9909@hotmail.com>
2016-11-11 20:26:03 -0500

    Remove lua as a vim dependency

--- install.sh
+++ install.sh
@@ -137,17 +137,10 @@ read ans
 if [[ $ans =~ ^[Yy]$ ]]; then
     sudo apt-get -y remove --purge vim vim-runtime vim-gnome vim-tiny vim-common vim-gui-common
     sudo apt-get -y build-dep vim-gnome
-    sudo apt-get -y install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev mercurial libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
+    sudo apt-get -y install python-dev ruby-dev libperl-dev mercurial libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
     sudo apt-get -y install npm nodejs
     sudo npm install -g npm
     sudo npm install -g instant-markdown-d
-    if [[ ! -d /usr/include/lua5.1/include ]]; then
-        sudo mkdir -p /usr/include/lua5.1/include
-        sudo mv /usr/include/lua5.1/*.h /usr/include/lua5.1/include/
-    fi
-    if [[ ! -f /usr/bin/luajit ]]; then
-        sudo ln -s /usr/bin/luajit-2.0.0-beta9 /usr/bin/luajit
-    fi
     cd "$HOME"
     git clone https://github.com/vim/vim "$HOME/vim"
     cd vim/src
@@ -158,17 +151,11 @@ if [[ $ans =~ ^[Yy]$ ]]; then
         --enable-pythoninterp \
         --with-python-config-dir=/usr/lib/python2.7/config \
         --enable-perlinterp \
-        --enable-luainterp \
-        --with-luajit \
         --enable-gui=auto \
         --enable-fail-if-missing \
-        --with-lua-prefix=/usr/include/lua5.1 \
         --enable-cscope
     make
     sudo make install
-    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
-    sh ./install.sh
-    rm install.sh
     sudo -v
 fi
 
