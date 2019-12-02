750c9bdb21a1fce005f1bc7c8b05a13752824122
James Wang <jameswang9909@hotmail.com>
2019-12-01 20:42:51 -0600

    polybar: Accommodate multiple monitors

--- .i3/polybar.sh
+++ .i3/polybar.sh
@@ -6,4 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar top &
+if type "xrandr"; then
+    for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR="$monitor" polybar --reload top &
+    done
+else
+    polybar --reload top &
+fi
